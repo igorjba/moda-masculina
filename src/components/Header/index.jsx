cf523139e85a2e28ea9e3b7ec18887b871b35b95
Igor <igorjbahia@gmai.com>
2023-05-23 15:01:47 -0300

    :sparkles: feat: Header concluído.

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,10 +1,17 @@
+import Logo from '../../assets/logo.svg'
 import './style.css'
 
 function Header() {
     return (
-        <div>
-            <h1>Header</h1>
-        </div>
+        <header>
+            <h1>MODA MASCULINA</h1>
+            <h2> SAPATOS SOCIAIS - CASUAIS - ESPORTE FINO</h2>
+            <img
+                className='logo'
+                src={Logo}
+                alt="Logo"
+            />
+        </header>
     )
 }
 
